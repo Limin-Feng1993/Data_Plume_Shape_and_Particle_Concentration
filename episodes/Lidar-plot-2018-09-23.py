629762aa6ca0ccb7ec8bbbd3bc887217ef015305
Limin-Feng1993 <fenglimin1993@gmail.com>
2019-07-10 13:51:08 +0800

    Add files

--- episodes/Lidar-plot-2018-09-23.py
+++ episodes/Lidar-plot-2018-09-23.py
@@ -82,16 +82,40 @@ def get_filename(filename):
   #return filepath,shotname,extension
   return shotname
 #########end 获取文件路径、文件名、后缀名############
-                        
-plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
+#Excel时间序列处理方法一
+def TS(x):
+    return (x - np.datetime64('1970-01-01T00:00:00Z'))/np.timedelta64(1, 's')
+    #return datetime.utcfromtimestamp(x.astype('O')/1e9)
+    #return datetime.fromtimestamp(x.tolist()/1e9)
+
+##Excel时间序列处理方法二
+def DT(x):
+    return datetime.utcfromtimestamp(x)
+    
+plt.rcParams['axes.unicode_minus']=False 
 mpl.rcParams['font.family'] = 'sans-serif'
 mpl.rcParams['font.sans-serif'] = 'Times New Roman'#中文除外的设置成New Roman，中文设置成宋体,NSimSun,
-#path=pathlib.Path('E:\\冯立民\\大气所\\工作\\日照市观测\\固定点雷达\\day\\')
-path=pathlib.Path('D:\\文章\\日照市烟羽模型\\events\\')
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = 'Times New Roman'#中文除外的设置成New Roman，中文设置成宋体,NSimSun,
+
+path=pathlib.Path('C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python36\\Scripts\\Sky_recog_pred\\small data\\')
+df=path/'2018-09 UVT.xlsx'
+U=pd.DataFrame(pd.read_excel(df, sheet_name=u'U'))
+V=pd.DataFrame(pd.read_excel(df, sheet_name=u'V'))
+TH=pd.DataFrame(pd.read_excel(df, sheet_name=u'TH'))
+
+X2=TH['Time']
+H3=TH['Height']
+X3=[DT(TS(i)) for i in X2]
+
+X4, Y4= np.meshgrid(X3,H3)
+
 fp='sphere-09-23.txt'
 ft='dust-09-23.txt'
 Ydata=np.loadtxt('Height.txt')#200行*1列 
 lie=[]
+
 
 with open(fp,'r') as f0:
 	for line in f0:
@@ -141,7 +165,10 @@ minval,maxval=0, 0.2
 #minval,maxval=0, 0.5
 #norm = matplotlib.colors.Normalize(vmin=0, vmax=500, clip=False)
 cs = plt.contourf(X,Y,Zdata, np.arange(minval, maxval, 0.001),cmap=plt.cm.get_cmap('jet'))#norm=norm, offset=-2, shrink=.92
-                                                                                                          
+plt.barbs(X4, Y4, U, V,barbcolor='white', flagcolor='r',linewidth=3, length=8, pivot='middle')      
+dateS=datetime(2018, 9, 23, 6, 00)
+dateE=datetime(2018, 9, 24, 18, 00)
+plt.xlim(dateS, dateE)                                                                                                  
 #设置时间标签显示格式
 plt.gca()
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))  
@@ -221,13 +248,18 @@ minval,maxval=0, 0.2
 #minval,maxval=0, 1.0
 #minval,maxval=0, 0.5
 #norm = matplotlib.colors.Normalize(vmin=0, vmax=200, clip=False)
+
 cs = plt.contourf(X,Y,Zdata,np.arange(minval,maxval, 0.001),  cmap=plt.cm.get_cmap('jet'))#norm=norm,shrink=.92,offset=-2,
-                                                                                                          
-#设置时间标签显示格式
+#plt.quiver(X4, Y4, U, V,color='white', width=0.005,scale=170)                                                                                                          
+plt.barbs(X4, Y4, U, V,barbcolor='white', flagcolor='r',linewidth=3, length=8, pivot='middle')      
+dateS=datetime(2018, 9, 23, 6, 00)
+dateE=datetime(2018, 9, 24, 18, 00)
+plt.xlim(dateS, dateE)
 plt.gca()
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))  
 plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=12))  #設置x軸主刻度間距
 plt.yticks(np.linspace(500,3000,6))
+
 #plt.yticks(np.logspace(500,3000,6, base=10), (100, 500, 1000, 2000, 3000))
 #ax.set_yticklabels( ('500', '1000', '1500', '2000', '2500', '3000'), fontproperties=font,fontsize=18)
 
@@ -253,6 +285,7 @@ plt.tight_layout()
 #plt.margins(0,0)
 #plt.subplots_adjust(left=0.2, bottom=0.2, right=0.8, top=0.8，hspace=0.2, wspace=0.3)
 filename=get_filename(fp)
+
 plt.savefig(str(filename)+'.png')
 plt.clf()
 print("ALL -> Finished OK")
---
+++ episodes/geany_run_script.bat
@@ -0,0 +1,6 @@
+python "Lidar-plot-2018-09-06.py"
+
+pause
+del "%0"
+
+pause
